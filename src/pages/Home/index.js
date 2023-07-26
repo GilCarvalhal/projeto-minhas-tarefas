5c2f2dd8ddc4987893156c80b9719a8802c92958
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-07-26 17:39:27 -0300

    criando a pagina home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,47 @@
+import { useState } from "react";
+import './home.css';
+import { Link } from "react-router-dom";
+
 export default function Home() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleLogin(e) {
+        e.preventDefault();
+
+        if (email !== '' && password !== '') {
+            alert('TESTE');
+        } else {
+            alert('Preencha todos os campos!');
+        }
+
+    }
+
+
     return (
-        <div>
+        <div className="home-container">
             <h1>
-                Página HOME
+                Lista de tarefas
             </h1>
+            <span>
+                Gerencia sua agenda de forma fácil
+            </span>
+
+            <form className="form" onSubmit={handleLogin}>
+                <input type="text"
+                    placeholder="Digite seu email..."
+                    value={email} onChange={(e) => setEmail(e.target.value)} />
+
+                <input autoComplete={false} type="password"
+                    placeholder="Digite sua senha..."
+                    value={password} onChange={(e) => setPassword(e.target.value)} />
+
+                <button type="submit">Acessar</button>
+            </form>
+
+            <Link className="button-link" to="/register">
+                Não possui uma conta? Cadastre-se
+            </Link>
         </div>
     )
 }
