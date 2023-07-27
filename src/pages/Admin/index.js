1a5b74b40f117b7c023794f283bec1b67f2b9f49
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-07-27 19:51:34 -0300

    registrando atividades no banco

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,16 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './admin.css'
 
-import { auth } from '../../firebaseConnection';
+
+import { auth, db } from '../../firebaseConnection';
 import { signOut } from 'firebase/auth';
+
+import { addDoc, collection } from 'firebase/firestore';
 
 export default function Admin() {
     const [tarefaInput, setTarefaInput] = useState('');
+    const [user, setUser] = useState({});
 
-    function handleRegister(e) {
+    useEffect(() => {
+        async function loadTarefas() {
+            const userDetail = localStorage.getItem("@detailUser")
+            setUser(JSON.parse(userDetail))
+        }
+        loadTarefas();
+    }, [])
+
+    async function handleRegister(e) {
         e.preventDefault();
 
-        alert('CLICOU');
+        if (tarefaInput === '') {
+            alert('Digite sua tarefa...');
+            return;
+        }
+        await addDoc(collection(db, "tarefas"), {
+            tarefa: tarefaInput,
+            created: new Date(),
+            userUid: user?.uid,
+        })
+            .then(() => {
+                console.log("TAREFA REGISTRADA");
+                setTarefaInput('');
+            })
+            .catch((error) => {
+                console.log("ERRO AO REGISTRAR " + error);
+            })
+
     }
 
     async function handleLogout() {
